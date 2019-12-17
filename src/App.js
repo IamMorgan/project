78ac1d4e62c6ce660bc7d7a4d31ec36d5edbdf6a
IamMorgan <bugidi002@users.noreply.github.com>
2019-12-17 15:27:20 +0300

    17.12

--- src/App.js
+++ src/App.js
@@ -17,23 +17,21 @@ import {BrowserRouter, Route} from 'react-router-dom';
 const App = (props) => {
 	
 	return (
-		<BrowserRouter>
-			<div className='app-wrapper'>
-				<Header />
-				<Navbar state={props.state.siteBar} />
-				<div className='app-wrapper-content'>
-					<Route path='/dialogs' render={() => <Dialogs state = {props.state.dialogsPage} 
-						icon={props.state.siteBar} 
-						dispatch = {props.dispatch}/>} />
-					<Route path='/profile' render={() => <Profile profilePage = {props.state.profilePage} 
-						dispatch = {props.dispatch} />} />
-					<Route path='/news' render={() => <News />} />
-					<Route path='/music' render={() => <Music />} />
-					<Route path='/settings' render={() => <Settings />} />
-				</div>
-				
+		<div className='app-wrapper'>
+			<Header />
+			<Navbar state={props.state.siteBar} />
+			<div className='app-wrapper-content'>
+				<Route path='/dialogs' render={() => <Dialogs state = {props.state.dialogsPage} 
+					icon={props.state.siteBar} 
+					dispatch = {props.dispatch}/>} />
+				<Route path='/profile' render={() => <Profile profilePage = {props.state.profilePage} 
+					dispatch = {props.dispatch} />} />
+				<Route path='/news' render={() => <News />} />
+				<Route path='/music' render={() => <Music />} />
+				<Route path='/settings' render={() => <Settings />} />
 			</div>
-		</BrowserRouter>
+			
+		</div>
 	);
 }
 
